6472328d54518b20cf29ad9cf92dbe74113316de
Dmitrijs Juskovs <bober12@inbox.lv>
2019-03-01 23:30:43 +0200

    Added possibility to login and publish messages

--- BotPrototype/Bot.cs
+++ BotPrototype/Bot.cs
@@ -12,10 +12,40 @@ namespace BotPrototype
 {
     public static class Bot
     {
+        private static string LoginToken = "";
+        private static string CurrentChannel = "";
+        private static bool isLoggedIn = false;
+        private static Socket currentSocket;
+
+        private static JObject FromObject = new JObject();
+
+        public static void SetPublishDetails(string login, int id, string token) {            
+            FromObject.Add("id", id);
+            FromObject.Add("name", login);
+            LoginToken = token;
+        }
+
         private static readonly Dictionary<String, Action<String, ChatMessage>> CommandList = new Dictionary<string, Action<string, ChatMessage>>();
 
         public static void AddCommand(string command, Action<String, ChatMessage> action) {
             CommandList.Add(command, action);
+        }
+
+        public static void PublishMessage(string message) {
+            if (CurrentChannel != "" && isLoggedIn) {
+                var MessageObject = new JObject();
+                MessageObject.Add("channel", "stream/" + CurrentChannel);
+                MessageObject.Add("from", FromObject);
+                MessageObject.Add("to", null);
+                MessageObject.Add("text", message);
+
+                Console.WriteLine($"Sending: {MessageObject.ToString()}");
+
+                currentSocket.Emit("/chat/publish", (b) =>
+                {
+                    Console.WriteLine($"Message Sent {b}");
+                }, MessageObject);
+            }
         }
 
         public static void Start(string channelId) {
@@ -31,6 +61,18 @@ namespace BotPrototype
             {
                 Console.WriteLine("Connected.");
                 JoinChat(socket, channelId);
+                if (LoginToken != "")
+                {
+                    var loginObject = new JObject();
+                    loginObject.Add("token", LoginToken);
+                    socket.Emit("/chat/login", (b) =>
+                    {
+                        ConnectionStatus msg = Newtonsoft.Json.JsonConvert.DeserializeObject<ConnectionStatus>(b.ToString());
+                        Console.WriteLine($"Login status: {msg.status}");
+                        isLoggedIn = true;
+                        currentSocket = socket;
+                    }, loginObject);
+                }
             });
         }
 
@@ -41,6 +83,7 @@ namespace BotPrototype
 
             socket.Emit("/chat/join", (b) =>
             {
+                CurrentChannel = channelId;
                 ConnectionStatus msg = Newtonsoft.Json.JsonConvert.DeserializeObject<ConnectionStatus>(b.ToString());
                 Console.WriteLine($"Joined to channel with status: {msg.status}");
             }, loginObject);
--- BotPrototype/Program.cs
+++ BotPrototype/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BotPrototype
@@ -17,11 +18,22 @@ namespace BotPrototype
             Bot.AddCommand("/123", (argument, msg) => {
                 Console.WriteLine($"была вызвана команда /123 с аргументом: {argument}");
             });
+           
             Bot.AddCommand("", (argument, msg) => {
                 Console.WriteLine($"{msg.from.name} сказал: {msg.text}");
+                if (msg.text.ToLower().Contains("тест")) {
+                    Thread.Sleep(10);
+                    Bot.PublishMessage("ответ (c) bot");
+                }
             });
+            
+            Bot.SetPublishDetails(
+                "...", // login
+                0, //id
+                "??" // token
+                );
 
-            Bot.Start("56592"); // 126885 - ViewersBW
+            Bot.Start("126885"); // 126885 - ViewersBW
             Console.ReadLine();
         }
 
